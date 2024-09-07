ed6de2232aa4ec324a8a6d8a29bf44d12cb753af
raphofuoku <raphaelofuoku16@gmail.com>
2024-09-07 12:38:33 +0100

    styling update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,33 @@
+import React, {useState} from 'react';
 import Nav from "./Nav";
-//import Logo from "./assets/Logo.svg";
-//import {Link} from 'react-router-dom';
+import Logo from "../assets/Logo.svg";
+import {Link} from 'react-router-dom';
+import '../style/Header.css';
 
 const Header = () => {
+    const [isMenuOpen, setMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
+    }
     return (
-        <header>
-            {/*<Link>
-            <img src={Logo} alt="logo" className="primary-logo"/>
-            </Link>*/}
-        <Nav />
+        <header className="header">
+            <Link>
+               <img src={Logo} alt="logo" className="primary-logo"/>
+            </Link>
+
+                 {/* Hamburger icon */}
+           <button className="hamburger-menu" onClick={toggleMenu}>
+                {/* Simple icon using divs */}
+                <div className={isMenuOpen ? 'bar1 open' : 'bar1'}></div>
+                <div className={isMenuOpen ? 'bar2 open' : 'bar2'}></div>
+                <div className={isMenuOpen ? 'bar3 open' : 'bar3'}></div>
+           </button>
+
+                {/* Navigation Menu */}
+            <nav className={isMenuOpen ? 'nav open' : 'nav'}>
+                <Nav />
+            </nav>
+
         </header>
     );
 }
