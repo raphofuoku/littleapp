069bf3455dbcf6d27238747331c71e5cbf8b9793
raphofuoku <raphaelofuoku16@gmail.com>
2024-09-07 11:09:34 +0100

    update project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import Nav from "./Nav";
+//import Logo from "./assets/Logo.svg";
+//import {Link} from 'react-router-dom';
 
 const Header = () => {
     return (
-        <>
+        <header>
+            {/*<Link>
+            <img src={Logo} alt="logo" className="primary-logo"/>
+            </Link>*/}
         <Nav />
-        </>
+        </header>
     );
 }
 
